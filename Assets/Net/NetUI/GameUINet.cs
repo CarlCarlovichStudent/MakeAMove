80270b90411af33e635c391b8cb926582e38e43a
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2022-11-29 18:10:02 +0100

    Net UI And Complete Connection
    Fixed UI and Capabilities for connecting online

--- Assets/Net/NetUI/GameUINet.cs
+++ Assets/Net/NetUI/GameUINet.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameUINet : MonoBehaviour
 {
     public static GameUINet Instance { set; get; }
+
+    [SerializeField] private Animator menuAnimation;
+    [SerializeField] private TMP_InputField addressInput;
+
+    public Server server;
+    public Client client;
 
     private void Awake()
     { 
@@ -15,11 +22,36 @@ public class GameUINet : MonoBehaviour
     //Buttons
     public void OnLocalGameButton()
     {
-        Debug.Log("yo");
+        menuAnimation.SetTrigger("InGameMenu");
+        server.Init(8007);
+        client.Init("127.0.0.1", 8007);
     }
     
     public void OnOnlineGameButton()
     {
-        Debug.Log("Blo");
+        menuAnimation.SetTrigger("OnlineMenu");
+    }
+    
+    public void OnOnlineHostButton()
+    {
+        server.Init(8007);
+        client.Init("127.0.0.1", 8007);
+        menuAnimation.SetTrigger("HostMenu");
+    }
+    
+    public void OnOnlineConnectButton()
+    {
+        client.Init(addressInput.text, 8007);
+    }
+    public void OnOnlineBackButton()
+    {
+        menuAnimation.SetTrigger("StartMenu");
+    }
+    
+    public void OnHostBackButton()
+    {
+        server.ShutDown();
+        client.ShutDown();
+        menuAnimation.SetTrigger("OnlineMenu");
     }
 }
